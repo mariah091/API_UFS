f58f1f6261d2fd7b831f376a3d4f28065926289b
mariah091 <mariahvitoriamartinsvha@gmail.com>
2025-08-26 07:50:42 -0400

    commit

--- apiUF/index.js
+++ apiUF/index.js
@@ -13,6 +13,28 @@ app.get('/ufs/:iduf', (req, res) => {
     res.json(uf);
 })
 
+app.get('/ufs/:iduf', (req, res) => {
+    const idUF = parseInt(req.params.iduf);
+    let mensagemErro = '';
+    let uf;
+
+
+    if(!(isNaN(idUF))) { /* se nao for um numero vai retonar verdadeiro (na vdd falso*/
+        uf = colecaoUf.find(u => u.id === idUF); /* uf vai ficar vazio se nao passar um id que tem */
+        if (!uf){ /* ! serve para verificar se tem algo dentro e ver se é verdadeiro ou falso*/
+            mensagemErro = 'UF não encontrada';
+        }
+    } else {
+        mensagemErro = 'Requisição inválida';
+    }
+
+    if (uf) {
+        res.json(uf);
+    } else {
+        res.status(404).json({ "erro": mensagemErro })
+    }
+});
+
 app.listen(8080, () => {
     let data = new Date();
     console.log('Servidor iniciado na porta 8080 em ' + data);
